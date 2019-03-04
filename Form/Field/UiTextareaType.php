79c91ceb4eab4a4e3ca0e9fcdb2d6923404af4dc
Jeandanyel <jd@eckinox.ca>
2019-03-04 13:27:54 +0000

    Added UiTextareaType

---
+++ Form/Field/UiTextareaType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Eckinox\Form\Field;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UiTextareaType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['pattern'] = null;
+        $view->vars['rows'] = $options['rows'];
+        $view->vars['toolbar'] = $options['toolbar'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return TextType::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'rows' => 10,
+            'toolbar' => '',
+        ));
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'uitextarea';
+    }
+}
