09b28a85fd79ca939bb507c0da0a25dea0293443
Pier-Luc Brassard <pl@eckinox.ca>
2019-06-17 11:05:59 -0400

    Traduction of all file in english

---
+++ Resources/translations/services.en.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "converter" => [
+        "errors" => [
+            "fileNotFound" => "The file can not be found.",
+            "fileUnreadable" => "User permissions do not allow reading of the file.",
+            "fileType" => "This file type is not supported. Supported file extensions are as follows: (%extensions%)."
+        ]
+    ]
+];
