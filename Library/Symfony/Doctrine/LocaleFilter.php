56759a92462f7546f7b658fc6b4fe89a560bb38c
Émile Perron <emilep@eckinox.ca>
2019-07-13 14:49:55 -0400

    Implemented the basic filters, listeners, helpers, services and templates for the Entity Translation feature.

---
+++ Library/Symfony/Doctrine/LocaleFilter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Eckinox\Library\Symfony\Doctrine;
+
+use Eckinox\Library\Symfony\EventListener\TranslationsListener;;
+use Doctrine\ORM\Mapping\ClassMetaData;
+use Doctrine\ORM\Query\Filter\SQLFilter;
+
+class LocaleFilter extends SQLFilter
+{
+    protected static $em = null;
+    protected static $locale = null;
+
+    public static function setEntityManager($em) {
+        static::$em = $em;
+    }
+
+    protected static function getEntityManager() {
+        return static::$em;
+    }
+
+    public static function setLocale($locale) {
+        static::$locale = $locale;
+    }
+
+    public static function getLocale() {
+        return static::$locale;
+    }
+
+    public static function disable() {
+        static::$em->getFilters()->disable('localeFilter');
+    }
+
+    public static function enable() {
+        static::$em->getFilters()->enable('localeFilter');
+    }
+
+    public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias)
+    {
+        # If the target entity isn't translatable, skip the locale filtering
+        if (!TranslationsListener::isClassTranslatableEntity($targetEntity->getName())) {
+            return '';
+        }
+
+        return sprintf('%1$s.locale = "%2$s" OR %1$s.locale = "" OR %1$s.locale IS NULL', $targetTableAlias, static::$locale);
+    }
+}
--- Library/Symfony/EventSubscriber/LocaleSubscriber.php
+++ Library/Symfony/EventSubscriber/LocaleSubscriber.php
@@ -4,13 +4,17 @@ namespace Eckinox\Library\Symfony\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Doctrine\ORM\EntityManagerInterface;
+use Eckinox\Library\Symfony\Doctrine\LocaleFilter;
 
 class LocaleSubscriber implements EventSubscriberInterface
 {
     private $defaultLocale;
+    private $em;
 
-    public function __construct($defaultLocale = 'fr_CA')
+    public function __construct(EntityManagerInterface $em, $defaultLocale = 'fr_CA')
     {
+        $this->em = $em;
         $this->defaultLocale = $defaultLocale;
     }
 
@@ -21,14 +25,21 @@ class LocaleSubscriber implements EventSubscriberInterface
             return;
         }
 
-        // try to see if the locale has been set as a _locale routing parameter
+        # Try to see if the locale has been set as a _locale routing parameter
         if ($locale = $request->query->get('_locale')) {
             $request->getSession()->set('_locale', $locale);
             $request->setLocale($locale);
         } else {
-            // if no explicit locale has been set on this request, use one from the session
+            # If no explicit locale has been set on this request, use one from the session
             $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
         }
+
+        # Enable the Doctrine SQL filter for entity locales
+        $filter = $this->em->getFilters()->enable('localeFilter');
+
+        # This is a fairly dirty workaround to injecting permanent data to the filter
+        LocaleFilter::setLocale($request->getLocale());
+        LocaleFilter::setEntityManager($this->em);
     }
 
     public static function getSubscribedEvents()
